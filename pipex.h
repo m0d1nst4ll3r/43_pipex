baea29f2ca7fe2f61b4248054c87c17c73441dfa
Raphael Pohlen <rapohlen@f1r10s11.clusters.42paris.fr>
2025-12-08 12:42:29 +0100

    prog desc comments accuracy

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: rapohlen <rapohlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 09:49:50 by rapohlen          #+#    #+#             */
-/*   Updated: 2025/12/08 11:55:35 by rapohlen         ###   ########.fr       */
+/*   Updated: 2025/12/08 12:41:28 by rapohlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,9 +58,11 @@
  *
  * A "here_doc" file will cause pipex to open stdin instead for heredoc input.
  *	It will wait for LIMITER to close stdin. If input is too lengthy for a
- *	pipe buffer size (taken to be 66536), a temporary file will be created.
+ *	pipe buffer size (taken to be 65536), a temporary file will be created.
  * Additionally, "here_doc" also causes pipex to ADD to file2 instead of
  *	truncating it. As such file2 is opened with O_WRONLY | O_CREAT | O_APPEND.
+ *
+ * Note that created files have their chmod set to 0644.
  *
  * Note: some commands built into the shell might work natively without a PATH,
  *	such as `pwd`, `cd`, `echo`, but pipex will not find them without a PATH.
